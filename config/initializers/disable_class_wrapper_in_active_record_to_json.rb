8d29d61a14b1e4255ea615bfc895d09b05d2b4b3
Pat Maddox <patmaddox@me.com>
2011-04-07 19:35:37 -0700

    tell engine to serve up static assets

--- config/initializers/disable_class_wrapper_in_active_record_to_json.rb
+++
@@ -1,7 +0,0 @@
-ActiveRecord::Base.include_root_in_json = false
-
-ActiveRecord::Base.class_eval do
-  def class_name
-    self.class.name
-  end
-end
--- lib/object_inspector/engine.rb
+++ lib/object_inspector/engine.rb
@@ -3,6 +3,20 @@ require 'rails'
 
 module ObjectInspector
   class Engine < Rails::Engine
-    engine_name :object_inspector
+    initializer "static assets" do |app|
+      if app.config.serve_static_assets
+        app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
+      end
+    end
+
+    initializer "f with activerecord" do |app|
+      ActiveRecord::Base.include_root_in_json = false
+
+      ActiveRecord::Base.class_eval do
+        def class_name
+          self.class.name
+        end
+      end
+    end
   end
 end
